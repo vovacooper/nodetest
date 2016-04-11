2e4f684d549c36cf3209749ce92262908b346433
vovacooper <vovacooper@gmail.com>
2016-04-11 18:12:05 +0300

    pack

--- config/config.js
+++ config/config.js
@@ -7,8 +7,8 @@ exports.config = function(){
     process.env['GRAPHENEDB_URL'] = "http://neo4j:Aa123456@localhost:7474"
     process.env['NEO4J_AUTH'] = ""
 
-    process.env['IMAGE_SERVICE_URL'] = "http://localhost:2000/img/"
-    // process.env['IMAGE_SERVICE_URL'] = "http://dozenlikes.com/img/"
+    // process.env['IMAGE_SERVICE_URL'] = "http://localhost:2000/img/"
+    process.env['IMAGE_SERVICE_URL'] = "http://localhost.com/img/"
 
     process.env['LOG_FILE'] = "/var/log/dozenlikes/nodejs.log"
 
