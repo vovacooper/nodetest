bfc67702b862cc03884fa719a9cb33a78a7024ab
vovacooper <vovacooper@gmail.com>
2016-04-20 09:23:25 +0300

    pack

--- services/service_lolwot_crawler.js
+++ services/service_lolwot_crawler.js
@@ -167,7 +167,7 @@ var crawl = function (i, lolwotlist, callback) {
 
 // Load From File
 jsonfile.readFile(file, function (err, obj) {
-    crawl(0, obj, function (err, res) {
+    crawl(4206, obj, function (err, res) {
         if (err) {
             winston.log('debug', err);
         }
